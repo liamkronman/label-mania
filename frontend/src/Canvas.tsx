aeb10ea83c2f4a39b499b9999749bb1a5b3ab711
liamkronman <liamkronman@gmail.com>
2022-08-22 11:17:26 -0400

    migration to typescript

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -1,13 +1,13 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, DOMElement } from 'react';
 
-const Canvas = props => {
-    const canvasRef = useRef(null);
-    const [currX, setCurrX] = useState(null);
-    const [currY, setCurrY] = useState(null);
-    const [fixedX, setFixedX] = useState(null);
-    const [fixedY, setFixedY] = useState(null);
-    const [finalX, setFinalX] = useState(null);
-    const [finalY, setFinalY] = useState(null);
+const Canvas = (props: any) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [currX, setCurrX] = useState<number | null>(null);
+    const [currY, setCurrY] = useState<number | null>(null);
+    const [fixedX, setFixedX] = useState<number | null>(null);
+    const [fixedY, setFixedY] = useState<number | null>(null);
+    const [finalX, setFinalX] = useState<number | null>(null);
+    const [finalY, setFinalY] = useState<number | null>(null);
     const [tracking, setTracking] = useState(false);
 
     useEffect(() => {
@@ -17,12 +17,12 @@ const Canvas = props => {
         });
         document.addEventListener('mousedown', (e) => {
             setTracking(true);
-            if (!fixedX) setFixedX(e.clientX);
-            if (!fixedY) setFixedY(e.clientY);
+            if (fixedX === null) setFixedX(e.clientX);
+            if (fixedY === null) setFixedY(e.clientY);
         });
         document.addEventListener('mouseup', (e) => {
             setTracking(false);
-            if (fixedX && fixedY && !finalX && !finalY) {
+            if (fixedX !== null && fixedY !== null && (finalX === null) && (finalY === null)) {
                 setFinalX(e.clientX);
                 setFinalY(e.clientY);
             }
@@ -30,8 +30,9 @@ const Canvas = props => {
     }, [fixedX, fixedY, finalX, finalY]);
 
     useEffect(() => {
-        if (((currX && currY) || (finalX && finalY)) && finalX && finalY) {
-            const canvas = canvasRef.current;
+        if (((currX !== null && currY !== null) || (finalX !== null && finalY !== null)) && (fixedX || fixedX == 0) && (fixedY || fixedY == 0)) {
+            
+            const canvas: any = canvasRef.current;
             const ctx = canvas.getContext('2d');
             ctx.canvas.width = window.innerWidth;
             ctx.canvas.height = window.innerHeight;
@@ -43,15 +44,16 @@ const Canvas = props => {
                 ctx.drawImage(img, 0, 0);
                 ctx.fillStyle = '#000';
                 ctx.beginPath();
-                if (finalX && finalY && !tracking) {
+                if (finalX !== null && finalY !== null && !tracking && fixedX !== null && fixedY !== null) {
                     ctx.rect(fixedX, fixedY, finalX - fixedX, finalY - fixedY);
-                } else {
+                } else if (fixedX !== null && fixedY !== null && currX !== null && currY !== null) {
                     ctx.rect(fixedX, fixedY, currX - fixedX, currY - fixedY);
                 }
                 ctx.stroke();
             }
         } else {
-            const canvas = canvasRef.current;
+            console.log("reached 2")
+            const canvas: any = canvasRef.current;
             const ctx = canvas.getContext('2d');
             ctx.canvas.width = window.innerWidth;
             ctx.canvas.height = window.innerHeight;
