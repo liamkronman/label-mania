0db80dde6cf08e0cc1b3df059a2ef2f66a94136d
frodakcin <huangsy222@gmail.com>
2022-08-22 11:41:16 -0400

    Combine position state variables into one dict

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -1,73 +1,124 @@
-import React, { useRef, useEffect, useState, DOMElement } from 'react';
+import React, { useRef, useEffect, useState } from "react";
+
+interface PositionData {
+	currX: number | null;
+	currY: number | null;
+	fixedX: number | null;
+	fixedY: number | null;
+	finalX: number | null;
+	finalY: number | null;
+}
 
 const Canvas = (props: any) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [currX, setCurrX] = useState<number | null>(null);
-    const [currY, setCurrY] = useState<number | null>(null);
-    const [fixedX, setFixedX] = useState<number | null>(null);
-    const [fixedY, setFixedY] = useState<number | null>(null);
-    const [finalX, setFinalX] = useState<number | null>(null);
-    const [finalY, setFinalY] = useState<number | null>(null);
-    const [tracking, setTracking] = useState(false);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [position, setPosition] = useState<PositionData>({
+		currX: null,
+		currY: null,
+		fixedX: null,
+		fixedY: null,
+		finalX: null,
+		finalY: null,
+	});
 
-    useEffect(() => {
-        document.addEventListener('mousemove', (e) => {
-            setCurrX(e.clientX);
-            setCurrY(e.clientY);
-        });
-        document.addEventListener('mousedown', (e) => {
-            setTracking(true);
-            if (fixedX === null) setFixedX(e.clientX);
-            if (fixedY === null) setFixedY(e.clientY);
-        });
-        document.addEventListener('mouseup', (e) => {
-            setTracking(false);
-            if (fixedX !== null && fixedY !== null && (finalX === null) && (finalY === null)) {
-                setFinalX(e.clientX);
-                setFinalY(e.clientY);
-            }
-        });
-    }, [fixedX, fixedY, finalX, finalY]);
+	useEffect(() => {
+		document.addEventListener("mousemove", (e) => {
+			setPosition((prev) => ({ ...prev, currX: e.clientX, currY: e.clientY }));
+		});
+		document.addEventListener("mousedown", (e) => {
+			setPosition((prev) =>
+				prev.finalX == null
+					? {
+							...prev,
+							fixedX: e.clientX,
+							fixedY: e.clientY,
+							finalX: null,
+							finalY: null,
+					  }
+					: prev
+			);
+		});
+		document.addEventListener("mouseup", (e) => {
+			setPosition((prev) =>
+				prev.fixedX !== null &&
+				prev.fixedY !== null &&
+				prev.finalX === null &&
+				prev.finalY === null
+					? { ...prev, finalX: e.clientX, finalY: e.clientY }
+					: prev
+			);
+		});
+	}, [position.fixedX, position.fixedY, position.finalX, position.finalY]);
 
-    useEffect(() => {
-        if (((currX !== null && currY !== null) || (finalX !== null && finalY !== null)) && (fixedX || fixedX == 0) && (fixedY || fixedY == 0)) {
-            
-            const canvas: any = canvasRef.current;
-            const ctx = canvas.getContext('2d');
-            ctx.canvas.width = window.innerWidth;
-            ctx.canvas.height = window.innerHeight;
-            // add image cat.webp to canvas
-            const img = new Image(ctx.canvas.width, ctx.canvas.height);
-            img.src = './cat.webp';
-            img.onload = () => {
-                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-                ctx.drawImage(img, 0, 0);
-                ctx.fillStyle = '#000';
-                ctx.beginPath();
-                if (finalX !== null && finalY !== null && !tracking && fixedX !== null && fixedY !== null) {
-                    ctx.rect(fixedX, fixedY, finalX - fixedX, finalY - fixedY);
-                } else if (fixedX !== null && fixedY !== null && currX !== null && currY !== null) {
-                    ctx.rect(fixedX, fixedY, currX - fixedX, currY - fixedY);
-                }
-                ctx.stroke();
-            }
-        } else {
-            console.log("reached 2")
-            const canvas: any = canvasRef.current;
-            const ctx = canvas.getContext('2d');
-            ctx.canvas.width = window.innerWidth;
-            ctx.canvas.height = window.innerHeight;
-            // add image cat.webp to canvas
-            const img = new Image(ctx.canvas.width, ctx.canvas.height);
-            img.src = './cat.webp';
-            img.onload = () => {
-                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-                ctx.drawImage(img, 0, 0);
-            }
-        }
-    }, [currX, currY, finalX, finalY]);
+	useEffect(() => {
+		if (
+			((position.currX !== null && position.currY !== null) ||
+				(position.finalX !== null && position.finalY !== null)) &&
+			(position.fixedX || position.fixedX == 0) &&
+			(position.fixedY || position.fixedY == 0)
+		) {
+			const canvas: any = canvasRef.current;
+			const ctx = canvas.getContext("2d");
+			ctx.canvas.width = window.innerWidth;
+			ctx.canvas.height = window.innerHeight;
+			// add image cat.webp to canvas
+			const img = new Image(ctx.canvas.width, ctx.canvas.height);
+			img.src = "./cat.webp";
+			img.onload = () => {
+				ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+				ctx.drawImage(img, 0, 0);
+				ctx.fillStyle = "#000";
+				ctx.beginPath();
+				if (
+					position.finalX !== null &&
+					position.finalY !== null &&
+					position.fixedX !== null &&
+					position.fixedY !== null
+				) {
+					ctx.rect(
+						position.fixedX,
+						position.fixedY,
+						position.finalX - position.fixedX,
+						position.finalY - position.fixedY
+					);
+				} else if (
+					position.fixedX !== null &&
+					position.fixedY !== null &&
+					position.currX !== null &&
+					position.currY !== null
+				) {
+					ctx.rect(
+						position.fixedX,
+						position.fixedY,
+						position.currX - position.fixedX,
+						position.currY - position.fixedY
+					);
+				}
+				ctx.stroke();
+			};
+		} else {
+			console.log("reached 2");
+			const canvas: any = canvasRef.current;
+			const ctx = canvas.getContext("2d");
+			ctx.canvas.width = window.innerWidth;
+			ctx.canvas.height = window.innerHeight;
+			// add image cat.webp to canvas
+			const img = new Image(ctx.canvas.width, ctx.canvas.height);
+			img.src = "./cat.webp";
+			img.onload = () => {
+				ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+				ctx.drawImage(img, 0, 0);
+			};
+		}
+	}, [
+		position.currX,
+		position.currY,
+		position.finalX,
+		position.finalY,
+		position.fixedX,
+		position.fixedY,
+	]);
 
-    return <canvas ref={canvasRef} {...props}/>;
-}
+	return <canvas ref={canvasRef} {...props} />;
+};
 
 export default Canvas;
