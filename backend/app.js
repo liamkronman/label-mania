fe92bde230c34b715aef12215fe7eaa82116711e
liamkronman <liamkronman@gmail.com>
2022-08-23 01:45:44 -0400

    working sequelize integration

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -21,6 +23,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("dotenv").config();
+
+const db = require('./models');
+db.sequelize.sync();
 
 require('./routes/auth.routes')(app);
 // require('./routes/user.routes')(app);
